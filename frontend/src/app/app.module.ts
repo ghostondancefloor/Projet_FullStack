bae70badb526fb7d24bf15f6e9bf12af98574583
Ayoub Mohammed Zouhri <Ayoub-mohammed.zouhri@etu.univ-nantes.fr>
2024-12-13 00:21:42 +0100

    layouts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -21,8 +21,6 @@ import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 @NgModule({
   declarations: [
@@ -33,9 +31,7 @@ import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component
     RegisterComponent,
     CalendarComponent,
     AddHabitDialogComponent,
-    SidebarComponent,
-    MainLayoutComponent,
-    AuthLayoutComponent
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
